6313ac24048703a6bf2a15d6e7b701d5ac91bdb6
Mang Wu <mang.wu@gmail.com>
2017-10-20 22:45:36 -0400

    check train test count

--- cdiscount/cd.py
+++ cdiscount/cd.py
@@ -1,4 +1,3 @@
-import xgboost as xgb
 import numpy as np
 import matplotlib.pyplot as plt
 import zipfile
@@ -7,18 +6,24 @@ from skimage.data import imread
 import io
 
 path = "/Users/apple/Documents/Programs/kaggle/porto/"
-path = "/Users/Public/Documents/Kaggle/porto/"
 path = "/Users/T162880/Documents/Programs/kaggle/cdiscount/"
+path = "/Users/Public/Documents/Kaggle/cdiscount/"
 
 train_example = "train_example.bson"
-data = bson.decode_file_iter(open(path+train_example, "rb"))
+train_file = "train.bson" #7069896
+test_file = "test.bson" #1768182
+#180*180 pic
+data = bson.decode_file_iter(open(path+train_file, "rb"))
 
+count = 0
 for item in data:
+    count += 1
+    if count == 1000: break
     product_id = item["_id"]
     category_id = item["category_id"]
     for index, img in enumerate(item["imgs"]):
         picture = imread(io.BytesIO(img["picture"]))
         filename = "{}-{}-{}.png".format(category_id, product_id, index+1)
         print(filename)
-        plt.imsave(path+filename, picture)
+        plt.imsave(path+"pic/"+filename, picture)
 
